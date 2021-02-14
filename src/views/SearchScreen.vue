8c9857204e61d5eaf72e349733c7df2429b202a5
ian lombu <citrakemurnian@gmail.com>
2021-02-14 21:39:20 +0700

    Fix Issues

---
+++ src/views/SearchScreen.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <router-link :to="{ name: 'Home'}">
+      <h1>POKEMON</h1>
+    </router-link>
+
+    <PokemonSearch />
+  </div>
+</template>
+
+<script>
+import PokemonSearch from '../components/PokemonSearch.vue'
+export default {
+    components: { PokemonSearch }
+}
+</script>
+
+<style>
+
+</style>
